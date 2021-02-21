11d2814d3967152c9274307b6d76bee087202896
Nam-Ki-Bok <nkb7714@naver.com>
2021-02-21 15:32:01 +0900

    [2021.02.21]

---
+++ Programmers/Tiling.py
@@ -0,0 +1,14 @@
+def solution(n):
+	dp = [0 for _ in range(60001)]
+	dp[1] = 1
+	dp[2] = 2
+
+	for i in range(3, n + 1):
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 1_000_000_007
+
+	return dp[n]
+
+
+if __name__ == "__main__":
+	n = 52424
+	print(solution(n))
